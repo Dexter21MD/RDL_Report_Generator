9a232545f6b36bb0037c394d9f75380e73215126
Dexter Krzeminski <dexter.krzeminski@gmail.com>
2024-02-29 20:06:48 +0100

    Clean up from trash code. For now it just prints xml element's path

--- RDL_Generator/generator.py
+++ RDL_Generator/generator.py
@@ -1,24 +1,32 @@
 
 from lxml import etree
-#C:\Users\Dexter\Desktop\pythonerinho\RDL Report Generator\templates\test.txt
-#"C:\\Users\\Dexter\\Desktop\\pythonerinho\\RDL Report Generator\\templates\\Template.rdl"
-
-#Load the .rdl file
-file_binary = open("C:\\Users\\Dexter\\Desktop\\pythonerinho\\RDL Report Generator\\templates\\Template.rdl", "rb")
-#new_parser = etree.XMLParser(remove_blank_text=True) 
-root = etree.parse(file_binary)
+from rdl_tree_manipulation import Rdl_Tree_Manipulation
 
 
-elements = root.xpath("//*")
-for element in elements:
-        if element.tag == "DataSources":
-            print(element.tag)
+# C:\Users\Dexter\Desktop\pythonerinho\RDL Report Generator\templates\test.txt
+# "C:\\Users\\Dexter\\Desktop\\pythonerinho\\RDL Report Generator\\templates\\Template.rdl"
+
+# open the .rdl file
+
+namespaces = {
+    'default': 'http://schemas.microsoft.com/sqlserver/reporting/2016/01/reportdefinition',
+    'rd': 'http://schemas.microsoft.com/SQLServer/reporting/reportdesigner',
+    'df': 'http://schemas.microsoft.com/sqlserver/reporting/2016/01/reportdefinition/defaultfontfamily'
+}
+
+file_binary = open(
+    "C:\\Users\\Dexter\\Desktop\\pythonerinho\\RDL Report Generator\\templates\\Template.rdl", "rb")
+
+tree = etree.parse(file_binary)
 
 
+x = Rdl_Tree_Manipulation(tree)
+
+without_namespaces_tree = x.get_rid_of_namespaces()
+
+# prints element's path to the console
+for element in without_namespaces_tree.iter():
+    print(tree.getpath(element))
 
 
 file_binary.close()
-
-
-
-
